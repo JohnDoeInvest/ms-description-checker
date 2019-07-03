36a9fd5903f8cd48abbad21734259ea3aaedac94
Henrik Olsson <henrikolsson@outlook.com>
2019-07-03 19:15:27 +0200

    Added service description validation

---
+++ src/checkSchema.js
@@ -0,0 +1,31 @@
+const Ajv = require('ajv')
+const ajv = Ajv({ allErrors: true })
+const path = require('path')
+const fs = require('fs')
+const glob = require('glob')
+const { logError } = require('./logger')
+
+// NOTE: There are not test for this, since the actual validation code is tested in the library.
+// I (Henrik) feel like testing that our schemas checks things correctly feels unnecessary.
+
+ajv.addSchema(require('./schema/service-description.schema.json'), 'service-description')
+  .addSchema(require('./schema/rest-api.schema.json'), 'rest-api')
+  .addSchema(require('./schema/env-vars.schema.json'), 'env-vars')
+  .addSchema(require('./schema/kafka.schema.json'), 'kafka')
+  .addSchema(require('./schema/sqlite.schema.json'), 'sqlite')
+
+module.exports = function (opts) {
+  const errors = []
+  const descriptionPaths = glob.sync(path.join(opts.srcPath, '/**/serviceDescription.json'))
+
+  for (let descriptionPath of descriptionPaths) {
+    var valid = ajv.validate('service-description', JSON.parse(fs.readFileSync(descriptionPath, 'utf-8')))
+
+    if (!valid) {
+      for (const error of ajv.errors) {
+        logError(errors, path.normalize(descriptionPath), error.message + ', ' + error.dataPath)
+      }
+    }
+  }
+  return errors
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 module.exports = function (opts) {
   const errors = [].concat(
+    require('./checkSchema')(opts),
     require('./checkKafka')(opts),
     require('./checkEnvVars')(opts)
   )
+
+  // TODO: Rewrite so that this file finds all services and then goes trough
+  // them with each of the checks? It would probably be quite simple.
 
   if (errors.length > 0) {
     return errors
